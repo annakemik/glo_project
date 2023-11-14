bb6757ea4fb7f73927a3685f03f1430063f49ce0
Anna Zubkova <annakemik@gmail.com>
2023-11-13 20:08:59 -0500

    Added lesson 2

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
-alert("Hello, my first project");
-console.log("Console message");
+const title = "Glo Project";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 500;
+const rollback = 10;
+const fullPrice = 1500;
+const adaptive = true;
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+const screensArray = screens.toLocaleLowerCase().split(" ");
+console.log(screensArray);
+
+console.log(
+  "Процент отката посреднику за работу " + fullPrice * (rollback / 100)
+);
