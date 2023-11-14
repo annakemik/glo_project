5d9d83acbc4f5e2586b3607e5661a747cc98bd46
Anna Zubkova <annakemik@gmail.com>
2023-11-14 17:42:41 -0500

    Added lesson 3

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const title = "Glo Project";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 500;
-const rollback = 10;
-const fullPrice = 1500;
-const adaptive = true;
+const title = prompt("Как называется ваш проект");
+const screens = prompt("Какие типа экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+const rollBack = 50;
+const adaptiveFromUser = prompt("Нужен ли адаптив на сайте?");
+const adaptiveString = adaptiveFromUser.toLocaleLowerCase().trim();
+let adaptive;
 
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+if (adaptiveString == "да") {
+  adaptive = true;
+} else if (adaptiveString == "нет") {
+  adaptive = false;
+} else {
+  alert("Ответ на вопрос про адаптив должен быть - Да или Нет.");
+  adaptive = false;
+}
 
-const screensArray = screens.toLocaleLowerCase().split(" ");
-console.log(screensArray);
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
 
-console.log(
-  "Процент отката посреднику за работу " + fullPrice * (rollback / 100)
-);
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log(fullPrice);
+
+const servicePercentPrice = Math.floor(fullPrice - rollBack);
+console.log(servicePercentPrice);
+
+switch (true) {
+  case fullPrice >= 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice >= 15000 && fullPrice < 30000:
+    console.log("Даем скидку в 5%");
+    break;
+  case fullPrice > 0 && fullPrice < 15000:
+    console.log("Скидка не предусмотрена");
+    break;
+  case fullPrice < 0:
+    console.log("Что-то пошло не так");
+    break;
+}
