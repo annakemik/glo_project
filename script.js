6b91d519a32908a787cf0f7ca5ed0513b0f2c5aa
Anna Zubkova <annakemik@gmail.com>
2023-11-16 09:22:29 -0500

    Part of lesson 3

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const title = prompt("Как называется ваш проект");
 const screens = prompt("Какие типа экранов нужно разработать?");
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
+let fullPrice;
 const rollBack = 50;
 const adaptiveFromUser = prompt("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+const servicePercentPrice = Math.floor(
+  fullPrice - fullPrice * (rollBack / 100)
+);
+
 const adaptiveString = adaptiveFromUser.toLocaleLowerCase().trim();
 let adaptive;
 
@@ -15,28 +24,29 @@ if (adaptiveString == "да") {
   adaptive = false;
 }
 
-const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = +prompt("Сколько это будет стоить?");
-const service2 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice2 = +prompt("Сколько это будет стоить?");
+// switch (true) {
+//   case fullPrice >= 30000:
+//     console.log("Даем скидку в 10%");
+//     break;
+//   case fullPrice >= 15000 && fullPrice < 30000:
+//     console.log("Даем скидку в 5%");
+//     break;
+//   case fullPrice > 0 && fullPrice < 15000:
+//     console.log("Скидка не предусмотрена");
+//     break;
+//   case fullPrice < 0:
+//     console.log("Что-то пошло не так");
+//     break;
+// }
 
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
+const getAllServierPrices = function () {
+  return servicePrice1 + servicePrice2;
+};
 
-const servicePercentPrice = Math.floor(fullPrice - rollBack);
-console.log(servicePercentPrice);
-
-switch (true) {
-  case fullPrice >= 30000:
-    console.log("Даем скидку в 10%");
-    break;
-  case fullPrice >= 15000 && fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break;
-  case fullPrice > 0 && fullPrice < 15000:
-    console.log("Скидка не предусмотрена");
-    break;
-  case fullPrice < 0:
-    console.log("Что-то пошло не так");
-    break;
+function getFullPrice() {
+  fullPrice = screenPrice + getAllServierPrices();
 }
+
+console.log(getAllServierPrices());
+getFullPrice();
+console.log(fullPrice);
