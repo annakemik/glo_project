a8e35c6e9bcf26de71195b839fcaa62c19ba9cfe
Anna Zubkova <annakemik@gmail.com>
2023-11-21 21:44:57 -0500

    Added lesson 5

--- script.js
+++ script.js
@@ -14,34 +14,54 @@ let servicePercentPrice;
 const isNumber = function (num) {
   return !isNaN(parseFloat(num) && isFinite(num));
 };
+
+const userCancel = function (userInput) {
+  return userInput === null;
+};
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
 };
 
 const asking = function () {
-  title = prompt("Как называется ваш проект");
-  screens = prompt("Какие типа экранов нужно разработать?");
+  title = prompt("Как называется ваш проект", "Rerf");
+  screens = prompt("Какие типа экранов нужно разработать?", "fdsfm fdsf");
   while (!isNumber(screenPrice)) {
     screenPrice = prompt("Сколько будет стоить данная работа?");
+    if (userCancel(screenPrice)) break;
+    screenPrice = +screenPrice.trim();
   }
   adaptive = confirm("Нужен ли адаптив на сайте?");
 };
 
 const getAllServicePrices = function () {
-  let sum = 0;
+  let total = 0;
+  let sum1;
+  let sum2;
   for (let i = 0; i < 2; i++) {
     if (i === 0) {
       service1 = prompt("Какой дополнительный тип услуги нужен?");
+      if (userCancel(service1)) return;
+      while (!isNumber(sum1)) {
+        sum1 = prompt("Сколько это будет стоить");
+        if (userCancel(sum1)) return;
+        sum1 = +sum1.trim();
+      }
     } else if (i === 1) {
       service2 = prompt("Какой дополнительный тип услуги нужен?");
+      if (userCancel(service2)) return;
+      while (!isNumber(sum2)) {
+        sum2 = prompt("Сколько это будет стоить");
+        if (userCancel(sum2)) return;
+        sum2 = +sum2.trim();
+      }
     }
-    sum += +prompt("Сколько это будет стоить");
+    total = sum1 + sum2;
   }
-  return sum;
+  return total;
 };
 
-function getFullPrice(price1, callback) {
-  return price1 + callback;
+function getFullPrice(price1, price2) {
+  return price1 + price2;
 }
 
 const getTitle = function (name) {
@@ -68,19 +88,15 @@ const getRollbackMessage = function (price) {
 
 asking();
 allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice(
-  screenPrice,
-  getAllServicePrices(servicePrice1, servicePrice2)
-);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
 servicePercentPrice = getServicePercentPrice(fullPrice, rollBack);
 
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log(allServicePrices);
+console.log("All service prices (доп услуги) = ", allServicePrices);
 console.log(getRollbackMessage(fullPrice));
-console.log(allServicePrices);
-console.log(fullPrice);
+console.log("Full price = ", fullPrice);
 console.log(getTitle(title));
-console.log(servicePercentPrice);
+console.log("Service percent price = ", servicePercentPrice);
